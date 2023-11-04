54a9c2325e69db27d5ac71002448939f1ed9dafe
cmgmage <147892342+cmgmage@users.noreply.github.com>
2023-11-04 06:23:48 +0000

    20231104_4 commit

---
+++ 20231104_4.py
@@ -0,0 +1,8 @@
+from tools import getStudent
+
+stu1 = getStudent(name="robort",chinese=78,math=90,english=92)
+print(stu1.name)
+print(stu1.chinese)
+print(stu1.english)
+print(stu1.math)
+print(stu1.sum())
